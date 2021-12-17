c2376eb09ab1296f3f56c4ee850180264640b0d9
MiniaczQ <jakub.motyka.2000@gmail.com>
2021-12-17 15:48:15 +0100

    re-added migrations

---
+++ lab2/Migrations/20211217144713_with-concurrency.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace lab1.Migrations
+{
+    public partial class withconcurrency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "timestamp",
+                table: "entries",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: false)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "timestamp",
+                table: "accepted_entries",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: false)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "timestamp",
+                table: "entries");
+
+            migrationBuilder.DropColumn(
+                name: "timestamp",
+                table: "accepted_entries");
+        }
+    }
+}
