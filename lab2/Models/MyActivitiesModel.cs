97255be7f0e5255e05483192d73463bde737051d
MiniaczQ <jakub.motyka.2000@gmail.com>
2021-12-17 01:35:31 +0100

    removed/kept old models

--- lab2/Models/MyActivitiesModel.cs
+++ lab2/Models/MyActivitiesModel.cs
@@ -6,26 +6,26 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace lab1.Models
 {
-    public class MyActivitiesModel
-    {
-        public MyActivitiesModel(String user) {
-            var activities = Entities.Activities.load();
-            activities.activities.RemoveAll(a => a.manager != user);
-            var reports = Entities.Report.getAll();
-            foreach (var report in reports)
-            {
-                foreach (var entry in report.acceptedEntries)
-                {
-                    foreach (var activity in activities.activities) {
-                        if (entry.code.Equals(activity.code)) {
-                            activity.budget -= entry.time;
-                        }
-                    }
-                }
-            }
-            this.Activities = activities;
-        }
-
-        public Activities Activities;
-    }
+    //public class MyActivitiesModel
+    //{
+    //    public MyActivitiesModel(string user) {
+    //        var activities = Entities.Activities.load();
+    //        activities.activities.RemoveAll(a => a.manager != user);
+    //        var reports = Entities.Report.getAll();
+    //        foreach (var report in reports)
+    //        {
+    //            foreach (var entry in report.acceptedEntries)
+    //            {
+    //                foreach (var activity in activities.activities) {
+    //                    if (entry.code.Equals(activity.code)) {
+    //                        activity.budget -= entry.time;
+    //                    }
+    //                }
+    //            }
+    //        }
+    //        this.Activities = activities;
+    //    }
+    //
+    //    public Activities Activities;
+    //}
 }
